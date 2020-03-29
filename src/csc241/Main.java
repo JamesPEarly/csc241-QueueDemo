0a2ebde29c2d1d3354bafd37c34b1c5d10b219ae
JamesPEarly <james.early@oswego.edu>
2020-03-29 08:32:56 -0400

    Add code from video

--- src/csc241/Main.java
+++ src/csc241/Main.java
@@ -6,14 +6,13 @@ public class Main {
 
         IntQueue iq1 = new IntQueue();
 
-        iq1.enqueue(12);
-        iq1.enqueue(24);
-        iq1.enqueue(36);
+        iq1.enqueue(5);
+        iq1.enqueue(20);
+        iq1.enqueue(15);
+        iq1.enqueue(37);
 
-        while (!iq1.isEmpty()){
+        while (!iq1.isEmpty()) {
             System.out.println("DEQUEUE: " + iq1.dequeue());
         }
-
-
     }
 }
