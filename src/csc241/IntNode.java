e54b2ff28215ecfc3259075afed1b24738010926
JamesPEarly <james.early@oswego.edu>
2020-03-29 08:33:13 -0400

    Add code from video

---
+++ src/csc241/IntNode.java
@@ -0,0 +1,12 @@
+package csc241;
+
+public class IntNode {
+    // Instance variables
+    int value;
+    IntNode next;
+
+    // Constructor
+    public IntNode(int value){
+        this.value = value;
+    }
+}
---
+++ src/csc241/IntQueue.java
@@ -0,0 +1,51 @@
+package csc241;
+
+public class IntQueue {
+
+    // Instance variables
+    IntNode front;
+    IntNode back;
+
+    // Constructor
+    public IntQueue(){
+        this.front = null;
+        this.back = null;
+    }
+
+    // isEmpty -- determine if queue is empty
+    public boolean isEmpty(){
+        return front == null && back == null;
+    }
+
+    // enqueue -- add element to the back
+    public void enqueue(int v){
+        IntNode temp = new IntNode(v);
+        if (back == null) {
+            // Adding first element
+            back = temp;
+            front = temp;
+        } else {
+            // Add new back element
+            back.next = temp;
+            back = temp;
+        }
+    }
+
+    // dequeue -- remove element from front
+    public Integer dequeue(){
+        if (front != null) {
+            IntNode temp = front;
+            front = front.next;
+            if (front == null) {
+                // removed the only element
+                back = null;
+            }
+            return temp.value;
+        } else {
+            // Queue empty
+            return null;
+        }
+    }
+
+
+}
